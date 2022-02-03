44fe9de715bfedda67e96c1f2d21a3f296e5facb
Alex Slocombe <alexslocombe@protonmail.com>
2022-02-03 01:56:16 +0000

    03/02/2022

--- Red Light, Green Light Game/RedLightGreenLight copy.py
+++ Red Light, Green Light Game/RedLightGreenLight copy.py
@@ -1,5 +1,6 @@
 #Ghost icon attributed to 'Freepik' via 'flaticon.com'
 
+import random
 import pygame
 pygame.init()
 
@@ -31,6 +32,53 @@ player2X = winWidth*0.75 - playerWidth/2
 player2Y = winHeight - 50
 player2X_change = 0
 player2Y_change = 0
+
+enemyWidth = 35
+enemyHeight = 35
+
+#Enemy 1 image
+enemy1Img = pygame.image.load('ghost.png')
+enemy1Img = pygame.transform.scale(enemy1Img, (enemyWidth, enemyHeight))
+enemy1X = random.randint(0, 164)
+enemy1Y = random.randint(100, 200)
+enemy1X_change = 0.03
+
+#Enemy 2 image
+enemy2Img = pygame.image.load('ghost.png')
+enemy2Img = pygame.transform.scale(enemy2Img, (enemyWidth, enemyHeight))
+enemy2X = random.randint(200, 365)
+enemy2Y = random.randint(100, 200)
+enemy2X_change = 0.03
+
+#Enemy 3 image
+enemy3Img = pygame.image.load('ghost.png')
+enemy3Img = pygame.transform.scale(enemy2Img, (enemyWidth, enemyHeight))
+enemy3X = random.randint(0, 164)
+enemy3Y = random.randint(250, 350)
+enemy3X_change = 0.03
+
+#Enemy 4 image
+enemy4Img = pygame.image.load('ghost.png')
+enemy4Img = pygame.transform.scale(enemy2Img, (enemyWidth, enemyHeight))
+enemy4X = random.randint(200, 365)
+enemy4Y = random.randint(250, 350)
+enemy4X_change = 0.03
+
+#Enemy 1 function
+def enemy1(x,y):
+    screen.blit(enemy1Img, (x,y))
+
+#Enemy 2 function
+def enemy2(x,y):
+    screen.blit(enemy2Img, (x,y))
+
+#Enemy 3 function
+def enemy3(x,y):
+    screen.blit(enemy3Img, (x,y))
+
+#Enemy 4 function
+def enemy4(x,y):
+    screen.blit(enemy4Img, (x,y))
 
 #Player 1 function
 def player1(x,y):
@@ -115,8 +163,47 @@ while running:
         player2X = winWidth/2 + 1
     elif player2X >= winWidth - playerWidth:
         player2X = winWidth - playerWidth
-    player2Y += player2Y_change
     if player2Y >= winHeight - playerHeight:
         player2Y = winHeight - playerHeight
+
+    #Enemy 1 movement
+    enemy1X += enemy1X_change
+    enemy1(enemy1X, enemy1Y)
+
+    #Enemy 1 boundaries
+    if enemy1X <= 0:
+        enemy1X_change = -enemy1X_change
+    elif enemy1X >= winWidth/2 - enemyWidth -1:
+        enemy1X_change = -enemy1X_change
+
+    #Enemy 2 movement
+    enemy2X += enemy2X_change
+    enemy2(enemy2X, enemy2Y)
+
+    #Enemy 2 boundaries
+    if enemy2X <= winWidth/2 + 1:
+        enemy2X_change = -enemy2X_change
+    elif enemy2X >= winWidth - enemyWidth:
+        enemy2X_change = -enemy2X_change
+
+    #Enemy 3 movement
+    enemy3X += enemy3X_change
+    enemy3(enemy3X, enemy3Y)
+
+    #Enemy 3 boundaries
+    if enemy3X <= 0:
+        enemy3X_change = -enemy3X_change
+    elif enemy3X >= winWidth/2 - enemyWidth -1:
+        enemy3X_change = -enemy3X_change
+
+    #Enemy 4 movement
+    enemy4X += enemy4X_change
+    enemy4(enemy4X, enemy4Y)
+
+    #Enemy 4 boundaries
+    if enemy4X <= winWidth/2 + 1:
+        enemy4X_change = -enemy4X_change
+    elif enemy4X >= winWidth - enemyWidth:
+        enemy4X_change = -enemy4X_change
 
     pygame.display.update()
